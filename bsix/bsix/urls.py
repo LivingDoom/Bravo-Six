e92c5d180dc70200b12938d198eaf3fb9df203b7
Josh Harmon <josh@joshharmon.me>
2019-12-12 22:29:21 -0800

    Add in templates for authentication

--- bsix/bsix/urls.py
+++ bsix/bsix/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/bsix/')),
     path('bsix/', include('app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
