ad6654949b864c389851ebf47eb43acc50c1431e
Josh Harmon <josh@joshharmon.me>
2019-12-12 17:40:32 -0800

    Port static pages to Django templates
    Ported:
     - Home
     - FAQ
     - Welfare info
    
    Views are in bsix/app/templates/app/
    
    base.html is the "base layout" view, which other pages will extend

--- bsix/app/views.py
+++ bsix/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'app/home.html')
+
+def faq(request):
+    return render(request, 'app/faq.html')
+
+def welfare(request):
+    return render(request, 'app/welfare.html')
