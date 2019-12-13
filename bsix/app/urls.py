c162b5cb6f2a3ae47f0329ff56cee62abb4770b7
Josh Harmon <josh@joshharmon.me>
2019-12-12 22:42:54 -0800

    Update urls.py

--- bsix/app/urls.py
+++ bsix/app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('faq', views.faq, name='faq'),
     path('welfare', views.welfare, name='welfare'),
-    path('apply', views.apply, name='apply')
+    path('apply', views.apply, name='apply'),
     path('unemploymentConfirmation', views.unemploymentConfirmation, name='unemploymentConfirmation'),
 ]
